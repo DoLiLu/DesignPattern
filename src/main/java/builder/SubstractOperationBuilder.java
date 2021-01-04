f6f8028546c1e76b91158adfd3c95cfd0027ea50
DESKTOP-T6O4081\lee <lsecret@naver.com>
2021-01-04 22:03:25 +0900

    Builder Pattern

---
+++ src/main/java/builder/SubstractOperationBuilder.java
@@ -0,0 +1,18 @@
+package builder;
+
+public class SubstractOperationBuilder extends AbstractOperationBuilder{
+
+    public SubstractOperationBuilder(int firstNumber, int secondNumber) {
+        super(firstNumber, secondNumber);
+    }
+
+    @Override
+    public int operate(int firstNumber, int secondNumber) {
+        return firstNumber - secondNumber;
+    }
+
+    @Override
+    public void buildOperator() {
+        result.append("-");
+    }
+}
